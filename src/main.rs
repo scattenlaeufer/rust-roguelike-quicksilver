51430de61cde499a0284018cab4417fe9b04abe2
Björn Guth <bjoern.guth@rwth-aachen.de>
2019-08-02 22:18:53 +0200

    Create a blank window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use quicksilver::{
+    geom::Vector,
+    lifecycle::{run, Settings, State, Window},
+    Result,
+};
+
+struct Game;
+
+impl State for Game {
+    /// Load the asset and initialise the game
+    fn new() -> Result<Self> {
+        Ok(Self)
+    }
+
+    /// Process keyboard and mouse, update the game state
+    fn update(&mut self, window: &mut Window) -> Result<()> {
+        Ok(())
+    }
+
+    /// Draw stuff on the screen
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        Ok(())
+    }
 }
+
+fn main() {
+    let settings = Settings {
+        ..Default::default()
+    };
+    run::<Game>("Quicksilver Roguelike", Vector::new(800, 600), settings);
+}
+
