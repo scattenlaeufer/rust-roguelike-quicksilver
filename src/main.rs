413651df947a766dbd8d65752968e144f0e4b444
Björn Guth <bjoern.guth@rwth-aachen.de>
2019-08-03 01:18:44 +0200

    Draw some text on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 use quicksilver::{
-    geom::Vector,
-    lifecycle::{run, Settings, State, Window},
-    Result,
+    geom::{Shape, Vector},
+    graphics::{Background::Img, Color, Font, FontStyle, Image},
+    lifecycle::{run, Asset, Settings, State, Window},
+    Future, Result,
 };
 
-struct Game;
+struct Game {
+    title: Asset<Image>,
+    mononoki_font_info: Asset<Image>,
+}
 
 impl State for Game {
     /// Load the asset and initialise the game
     fn new() -> Result<Self> {
-        Ok(Self)
+        let font_mononoki = "mononoki-Regular.ttf";
+
+        let title = Asset::new(Font::load(font_mononoki).and_then(|font| {
+            font.render("Quicksilver Roguelike", &FontStyle::new(72.0, Color::BLACK))
+        }));
+
+        let mononoki_font_info = Asset::new(Font::load(font_mononoki).and_then(|font| {
+            font.render(
+                "Mononoki font by Matthias Tellen, terms: SIL Open Font License 1.1",
+                &FontStyle::new(20.0, Color::BLACK),
+            )
+        }));
+
+        Ok(Self {
+            title,
+            mononoki_font_info,
+        })
     }
 
     /// Process keyboard and mouse, update the game state
@@ -19,6 +39,24 @@ impl State for Game {
 
     /// Draw stuff on the screen
     fn draw(&mut self, window: &mut Window) -> Result<()> {
+        window.clear(Color::WHITE)?;
+
+        self.title.execute(|image| {
+            window.draw(
+                &image.area().with_center((window.screen_size().x as i32 / 2, 40)),
+                Img(&image),
+                );
+            Ok(())
+        })?;
+
+        self.mononoki_font_info.execute(|image| {
+            window.draw(
+                &image.area().translate((2, window.screen_size().y as i32 - 60)),
+                Img(&image),
+                );
+            Ok(())
+        })?;
+
         Ok(())
     }
 }
@@ -29,4 +67,3 @@ fn main() {
     };
     run::<Game>("Quicksilver Roguelike", Vector::new(800, 600), settings);
 }
-
