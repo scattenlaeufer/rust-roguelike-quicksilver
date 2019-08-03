e4352416f4291ab6e75d8242338b72a681727a83
Björn Guth <bjoern.guth@rwth-aachen.de>
2019-08-03 20:41:29 +0200

    Add a square font for the map

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::collections::HashMap;
 struct Game {
     title: Asset<Image>,
     mononoki_font_info: Asset<Image>,
+    square_font_info: Asset<Image>,
     map_size: Vector,
     map: Vec<Tile>,
     entities: Vec<Entity>,
@@ -22,6 +23,7 @@ impl State for Game {
     /// Load the asset and initialise the game
     fn new() -> Result<Self> {
         let font_mononoki = "mononoki-Regular.ttf";
+        let font_square = "square.ttf";
         let map_size = Vector::new(20, 15);
         let map = generate_map(map_size);
         let mut entities = generate_entities();
@@ -45,9 +47,16 @@ impl State for Game {
             )
         }));
 
+        let square_font_info = Asset::new(Font::load(font_mononoki).and_then(move |font| {
+            font.render(
+                "Square font by Wouter Van Oortmerssen, terms: CC BY 3.0",
+                &FontStyle::new(20.0, Color::BLACK),
+            )
+        }));
+
         let game_glyphs = "#@g.%";
-        let tile_size_px = Vector::new(12, 24);
-        let tileset = Asset::new(Font::load(font_mononoki).and_then(move |text| {
+        let tile_size_px = Vector::new(24, 24);
+        let tileset = Asset::new(Font::load(font_square).and_then(move |text| {
             let tiles = text
                 .render(game_glyphs, &FontStyle::new(tile_size_px.y, Color::WHITE))
                 .expect("Could not render the font tileset.");
@@ -63,6 +72,7 @@ impl State for Game {
         Ok(Self {
             title,
             mononoki_font_info,
+            square_font_info,
             map_size,
             map,
             entities,
@@ -98,6 +108,16 @@ impl State for Game {
                 &image
                     .area()
                     .translate((2, window.screen_size().y as i32 - 60)),
+                Img(&image),
+            );
+            Ok(())
+        })?;
+
+        self.square_font_info.execute(|image| {
+            window.draw(
+                &image
+                    .area()
+                    .translate((2, window.screen_size().y as i32 - 30)),
                 Img(&image),
             );
             Ok(())
